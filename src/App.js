ea8c3368221235d7597aeb7af500b7e2f3c27e4d
BETHANY FOLINO <bethany.folino@gmail.com>
2021-03-06 15:12:29 -0800

    Finished part 1 - added simple button that fires console.log when clicked

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  function handleToggle() {
+    fetch("https://api.github.com/users/BethanyFolino");
+    console.log("Made you click!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleToggle}>Toggle User</button>
     </div>
   );
 }

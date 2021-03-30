aed16f87299468d33c489aca5b61096ca848c760
BETHANY FOLINO <bethany.folino@gmail.com>
2021-03-29 22:52:27 -0700

    Added Bootstrap components and added fetch request to handleToggle function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Card } from "bootstrap";
 
 function App() {
+  this.state = { user: {}, active: false };
+
   function handleToggle() {
-    fetch("https://api.github.com/users/BethanyFolino");
     console.log("Made you click!");
+    fetch("https://api.github.com/users/BethanyFolino").then(
+      this.state.user == "BethanyFolino"
+    );
+    !this.state.active;
   }
 
   return (
     <div className="App">
-      <button onClick={handleToggle}>Toggle User</button>
+      <Card style={{ width: "18rem" }}>
+        <Card.Body>
+          <Card.Title>{this.state.avatar_url}</Card.Title>
+          <Card.Subtitle>{this.state.user}</Card.Subtitle>
+        </Card.Body>
+        <Button variant="info" onClick={handleToggle}>
+          Toggle User
+        </Button>
+      </Card>
     </div>
   );
 }
